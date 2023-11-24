f4cb1fc4070de34d4ff2d442e7602fe756718751
nblancoz <nblancozappala@gmail.com>
2023-11-24 12:36:44 +0100

    added products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { ProductContext } from "../../context/ProductContext/ProductState";
+import "./Products.scss"
+import { Link } from "react-router-dom";
 
 const Products = () => {
   const { products, getProducts } = useContext(ProductContext);
@@ -9,13 +11,17 @@ const Products = () => {
   }, []);
 
   return (
-    <div>
-      <h1>hola</h1>
+    <div className="container">
+      <h1>This are the products available:</h1>
+      <Link><span>Order Low-High</span></Link>
+      <Link><span>Order High-Low</span></Link>
       {products.map((product) => {
         return (
           <div key={product.id}>
             <div>
-              <h2>{product.name}</h2>
+              <h2>Name: {product.name}</h2>
+              <h3>Description: {product.description}</h3>
+              <h3>Price: {product.price}€</h3>
             </div>
           </div>
         );
