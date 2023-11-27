5da3dc14ee0a11d3168b4febf1399afca58aa336
nblancoz <nblancozappala@gmail.com>
2023-11-27 22:05:46 +0100

    footer improved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,34 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>footer</p>
-      <p>Find a store</p>
-      <p>Feedback</p>
-      <p>Discounts</p>
-      <p>Help</p>
-      <p>Work with us</p>
-      <p>Become a member</p>
+      <div className="footer1">
+        <p>Find a store</p>
+        <p>Feedback</p>
+        <p>Work with us</p>
+        <p>About Us</p>
+      </div>
+      <div className="footer2">
+        <p>Help</p>
+        <p>Order Status</p>
+        <p>Shipping & Delivery</p>
+        <p>Payment Options</p>
+        <p>Cancel Order</p>
+        <p>Contact Us</p>
+      </div>
+      <div className="footer3">
+        <p>News</p>
+        <p>Discounts</p>
+        <p>Students</p>
+        <p>Teachers</p>
+        <p>Birthdays</p>
+      </div>
+      <div className="logos">
+        <img src="../src/assets/phone-logo.avif" alt="" />
+        <img src="../src/assets/whatsapp-logo.png" alt="" />
+        <img src="../src/assets/gmail-logo.jpeg" alt="" />
+        <img src="../src/assets/x-logo.avif" alt="" />
+        <img src="../src/assets/ig-logo.webp" alt="" />
+      </div>
     </div>
   );
 };
