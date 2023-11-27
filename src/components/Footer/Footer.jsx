dfd7ec68b43d8a0c6645e555d48c6611730ccfb2
nblancoz <nblancozappala@gmail.com>
2023-11-27 16:36:18 +0100

    added more products, footer modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,12 @@ const Footer = () => {
   return (
     <div className="footer">
       <p>footer</p>
+      <p>Find a store</p>
+      <p>Feedback</p>
+      <p>Discounts</p>
+      <p>Help</p>
+      <p>Work with us</p>
+      <p>Become a member</p>
     </div>
   );
 };
