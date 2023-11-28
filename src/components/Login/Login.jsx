eefcfd2a4edeedbc3595b9fbf6bd4db05a253ead
nblancoz <nblancozappala@gmail.com>
2023-11-28 21:53:23 +0100

    login scss modified

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -23,40 +23,46 @@ const Login = () => {
   };
 
   return (
-    <div className="login">
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please enter your email" }]}
+    <>
+      <h3>Login:</h3>
+      <div className="login">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          className="form"
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please enter your email" }]}
+            className="input"
+          >
+            <Input className="input" />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please enter your password" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please enter your password" }]}
+            className="input"
+          >
+            <Input.Password className="input" />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-      <p>{msg}</p>
-    </div>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit" className="button">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        <p>{msg}</p>
+      </div>
+    </>
   );
 };
 
